38d37e6b30daa95d4981db89dcbe6cf55957ae26
Markin2011 <markinw2011@gmail.com>
2024-04-06 18:21:35 +0700

    .

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const morgan = require('morgan');
 const debug = require('debug')('app');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/",(req,res)=>{
     
