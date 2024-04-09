8a008a2eba2296c7d9c2ba564817b1fb9cfbea6d
Markin2011 <markinw2011@gmail.com>
2024-04-09 18:08:03 +0700

    add products

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-const express = require('express');
-const chalk = require('chalk');
-const morgan = require('morgan');
-const debug = require('debug')('app');
+const express = require("express");
+const chalk = require("chalk");
+const morgan = require("morgan");
+const debug = require("debug")("app");
 const app = express();
 const PORT = process.env.PORT || 4000;
-const path = require('path');
+const path = require("path");
+const productRounter = express.Router();
 
-app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")))
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.set("views","./src/views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
-    
-    res.render("index",{username: 'what555+', customers:["jack","ben","wolf"]}); 
-})
+productRounter.route("/").get((req, res) => {
+  res.render("Products",{
+    products: [
+        {productTitle:'นำเยาลางจาน',productDescription:'ล้างสะอาด',productPrice: 40},
+        {productTitle:'นำเยาลางจาน2',productDescription:'ล้างสะอาด2',productPrice: 42},
+        {productTitle:'นำเยาลางจาน3',productDescription:'ล้างสะอาด3',productPrice: 43},
+        {productTitle:'นำเยาลางจาน4',productDescription:'ล้างสะอาด4',productPrice: 44},
+    ]
+  });
+});
+app.use("/products", productRounter);
 
-app.listen(PORT,()=>{
-    debug("listening on port" + chalk.green(" : "+PORT));
-})
+app.get("/", (req, res) => {
+  res.render("index", {
+    username: "what555+",
+    customers: ["jack", "ben", "wolf"],
+  });
+});
+
+app.listen(PORT, () => {
+  debug("listening on port" + chalk.green(" : " + PORT));
+});
